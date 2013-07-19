fc698611bc529e280d4d0d451959d10551e4206f
Cory Benfield <lukasaoz@gmail.com>
2013-07-19 13:52:26 +0100

    Put back some outdated credential options.

--- server/server/settings.py
+++ server/server/settings.py
@@ -132,6 +132,8 @@ INSTALLED_APPS = (
 # We're not really that concerned about security, so this should be fine.
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.CryptPasswordHasher',
 )
