588473f2d667e931d1f4dc1bddca13a7e62bf036
Cory Benfield <lukasaoz@gmail.com>
2013-10-07 18:15:42 +0100

    Remove now invalid asynchronicity warning.

--- tornado_server/db/sqlite.py
+++ tornado_server/db/sqlite.py
@@ -4,12 +4,6 @@ tornado_server.db.sqlite
 ~~~~~~~~~~~~~~~~~~~~~~~~
 
 This file implements the Sqlite portion of the database abstraction.
-
-Please note: the Sqlite abstraction is NOT asynchronous. This differs from the
-Postgres abstraction, which is. This should not be a problem: we do not intend
-to use Sqlite in production. However, it's worth bearing in mind that testing
-against Sqlite can potentially hide some production-scope bugs. You are
-hereby warned.
 """
 import sqlite3
 
